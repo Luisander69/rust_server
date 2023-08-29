e990050e368960e638f790c6f46e6d3f8d159a9f
luisander69 <lui_stefany@yahoo.com>
2023-08-29 15:51:56 -0400

    After arrays

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+#![allow(unused_variables)]
+use http::Method;
+use http::Request;
+use server::Server;
+
+mod http;
+mod server;
+fn main() {
+    let get = Method::GET;
+    let delete = Method::DELETE;
+    let post = Method::POST;
+    let put = Method::PUT;
+    let server = Server::new("127.0.0.1:8080".to_string());
+    server.run();
+}
