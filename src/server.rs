7369fa45082422e13e11c8d5a14a98a417a6c139
luisander69 <lui_stefany@yahoo.com>
2023-09-02 11:21:47 -0400

    Custom Error

--- src/server.rs
+++ src/server.rs
@@ -11,26 +11,26 @@ impl Server {
     pub fn new(addr: String) -> Self {
         Self { addr }
     }
-    pub fn run(self){
+    pub fn run(self) {
         println!("Listening on {}", self.addr);
         let listener = TcpListener::bind(&self.addr).unwrap();
-        loop{
-            match listener.accept(){
+        loop {
+            match listener.accept() {
                 Ok((mut stream, addr)) => {
                     let mut buffer = [0; 1024];
-                    match stream.read(&mut buffer){
-                    Ok(_) => {
-                    println!("Received a request, {}", String::from_utf8_lossy(&buffer));
-                    Request::try_from(&buffer[..]);
-    let res: Result<Request, > = &buffer[..].try_into(); 
-                    },
-                    Err(e) =>println!("Failed to read from connection {}", e),
+                    match stream.read(&mut buffer) {
+                        Ok(_) => {
+                            println!("Received a request, {}", String::from_utf8_lossy(&buffer));
+                            match Request::try_from(&buffer[..]) {
+                                Ok(request) =>{},
+                                Err(e) => println!("Failed to parse request, {}", e),
+                            }
+                        }
+                        Err(e) => println!("Failed to read from connection {}", e),
                     }
-                    
-                },
+                }
                 Err(e) => println!("Error: {}", e),
             }
         }
-
     }
 }
