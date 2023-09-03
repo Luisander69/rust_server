7076095f547423dd9c6b7eaaf25c8518a4a4b1b3
luisander69 <lui_stefany@yahoo.com>
2023-09-03 10:16:48 -0400

    added error handling and converting options into results

--- src/http/request.rs
+++ src/http/request.rs
@@ -2,28 +2,46 @@ use super::method::Method;
 use std::convert::TryFrom;
 use std::error::Error;
 use std::fmt::{Debug, Display, Formatter, Result as FmtResult};
+use std::str;
+use std::str::Utf8Error;
 pub struct Request {
     path: String,
     query_string: Option<String>,
     method: Method,
 }
 
-impl TryFrom<&[u8]> for Request{
+impl TryFrom<&[u8]> for Request {
     type Error = ParseError;
-    fn try_from(buf: &[u8]) -> Result<Self, Self::Error>{
-        unimplemented!()
+    fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
+        let request = str::from_utf8(buf)?;
+
+        let (metod, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        let (path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        let (protocol, _) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        if protocol != "HTTP/1.1"{
+            return Err(ParseError::InvalidProtocol)
+        }
+        unimplemented!();
     }
 }
 
-pub enum ParseError{
+fn get_next_word(request: &str) -> Option<(&str, &str)> {
+    for (i, c) in request.chars().enumerate() {
+        if c == ' ' || c == '\r'{
+            return Some((&request[..i], &request[i+1..]))
+        }
+    }
+    None
+}
+pub enum ParseError {
     InvalidRequest,
     InvalidEncoding,
     InvalidProtocol,
     InvalidMethod,
 }
-impl ParseError{
-    fn message(&self) -> &str{
-        match self{
+impl ParseError {
+    fn message(&self) -> &str {
+        match self {
             Self::InvalidRequest => "Invalid Request",
             Self::InvalidEncoding => "Invalid Encoding",
             Self::InvalidProtocol => "Invalid Protocol",
@@ -31,17 +49,21 @@ impl ParseError{
         }
     }
 }
-impl Display for ParseError{
-    fn fmt(&self, f: &mut Formatter) -> FmtResult{
+impl From<Utf8Error> for ParseError {
+    fn from(_: Utf8Error) -> Self {
+        Self::InvalidEncoding
+    }
+}
+
+impl Display for ParseError {
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
         write!(f, "{}", self.message())
     }
 }
-impl Debug for ParseError{
-    fn fmt(&self, f: &mut Formatter) -> FmtResult{
+impl Debug for ParseError {
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
         write!(f, "{}", self.message())
     }
 }
 
-impl Error for ParseError{
-
-}
+impl Error for ParseError {}
